a4ec8a96745c371dcd9f7a82e493f26905ffe967
Alex Searle <sas073@bucknell.edu>
2026-01-30 19:01:43 -0500

    Added Indicators

--- src/f1_dashboard/dashboard.py
+++ src/f1_dashboard/dashboard.py
@@ -42,13 +42,19 @@ def get_engine() -> Engine:
 
 
 def get_data():
+    """
+    This method gets all of the data need for the dashboard
+    """
     engine = get_engine()
-    return pd.read_sql("SELECT * FROM processed_race_data", engine), pd.read_sql(
-        "SELECT * FROM drivers", engine
+    return (
+        pd.read_sql("SELECT * FROM processed_race_data", engine),
+        pd.read_sql("SELECT * FROM drivers", engine),
+        pd.read_sql("SELECT * FROM race_results", engine),
+        pd.read_sql("SELECT * FROM predictions", engine),
     )
 
 
-df, drivers = get_data()
+df, drivers, df_results, df_predictions = get_data()
 driver_finder = drivers.set_index("driverId", drop=False)
 
 driver_names = drivers.set_index("driverRef")
@@ -67,6 +73,12 @@ driver_picker = pn.widgets.Select(
     options=driver_names[["driverId"]].T.to_dict("records")[0],
 )
 
+top_3s = pn.indicators.Number(name="Top 3 Percentage", value=0, format="{value:.2%}")
+
+wins = pn.indicators.Number(name="Wins", value=0)
+
+top_5s = pn.indicators.Number(name="Top 5 Finishes", value=0)
+
 
 def update_driver_picker(year):
     driver_ids = df[df["year"] == year.new]
@@ -79,17 +91,32 @@ def update_driver_picker(year):
     )[0]
 
     driver_picker.options = driver_dict
-    print("Updated")
 
 
 def update_plot(driver, year):
+    temp_df = df[(df["driverId"] == driver) & (df["year"] == year)]
     fig = px.line(
-        df[(df["driverId"] == driver) & (df["year"] == year)],
+        temp_df,
         x="round",
         y="top3_driver_season_percentage",
     )
     fig.update_traces(mode="lines+markers", marker=dict(size=10), line=dict(width=4))
     fig.layout.autosize = True
+
+    top_3s.value = temp_df.iloc[-1]["top3_driver_season_percentage"]
+
+    temp_results = df_results[
+        (df_results["driverId"] == driver) & (df_results["year"] == year)
+    ]
+
+    wins_num = (temp_results["positionOrder"] == 1).sum()
+
+    top_5_num = (
+        (temp_results["positionOrder"] > 0) & (temp_results["positionOrder"] <= 5)
+    ).sum()
+
+    wins.value = wins_num
+    top_5s.value = top_5_num
 
     return fig
 
@@ -99,8 +126,33 @@ year_picker.param.trigger("value")
 
 plot = pn.bind(update_plot, driver_picker, year_picker)
 
+indicator_style = {"width": "250px", "height": "150px", "justify-content": "center"}
+
+indicator_layout = pn.FlexBox(
+    *[
+        pn.Card(
+            top_3s,
+            hide_header=True,
+            styles=indicator_style,
+        ),
+        pn.Card(
+            wins,
+            hide_header=True,
+            styles=indicator_style,
+        ),
+        pn.Card(
+            top_5s,
+            hide_header=True,
+            styles=indicator_style,
+        ),
+    ],
+    align_content="space-evenly",
+    gap="50px",
+    justify_content="center",
+)
+
 pn.template.FastListTemplate(
     title="F1 Data Dashboard",
     sidebar=["# Inputs", year_picker, driver_picker],
-    main=["### Plot", plot],
+    main=["### Plot", indicator_layout, plot],
 ).servable()
